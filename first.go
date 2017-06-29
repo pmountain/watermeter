b8ecc43e889b407b7ad85ff5ee912cb50e571d9f
Peter Mountain <pmountain@MountainCloudInformationSecurity.com>
2017-06-29 22:09:04 +0200

    Final change for tonight

--- first.go
+++ first.go
@@ -4,4 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Printf("This is Berliner Strasse 36, how is the ACAB Architecture and the SIEM?\n")
+	fmt.Printf("Here is a second line for you. How about that dude?\n")
 }
