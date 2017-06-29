9243668d178b05e73bed68357f8b0d911a71835b
Peter Mountain <pmountain@MountainCloudInformationSecurity.com>
2017-06-29 21:47:30 +0200

    Changed the text

--- first.go
+++ first.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("This is Zalando, how is the ACAB Architecture?\n")
+	fmt.Printf("This is Berliner Strasse 36, how is the ACAB Architecture?\n")
 }
